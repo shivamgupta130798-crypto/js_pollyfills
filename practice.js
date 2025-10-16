e27583fb1fb1a50a722ec05020cf74f019890e7d
Shivam Gupta <shivamgupta130798@gmail.com>
2025-10-16 23:28:39 +0530

    test consoled

--- practice.js
+++ practice.js
@@ -1,10 +1,9 @@
 let arr = [ 10, 8, 5, 4, 3, 2, 1]
 
 let reduceRes = arr.reduce((acc, curr) => acc * curr, 1)
-console.log(reduceRes);
+ 
 
-
-Array.prototype.map = function (cb){
+Array.prototype.myMap = function (cb){
     let temp =[]
     for(let i =0; i < this.length; i++){
         let newValue = cb(this[i], i, this)
@@ -13,6 +12,13 @@ Array.prototype.map = function (cb){
     return temp
 }
 
-let res = arr.map(i => i + i)
+let res = arr.myMap(i => i + i)
 
-console.log(res)
+const wordCount = (str) => str.trim().split(/\s+/).length;
+
+
+
+console.log('a')
+setTimeout(()=> console.log('b'),0)
+Promise.resolve().then(console.log('c'))
+console.log('d')
